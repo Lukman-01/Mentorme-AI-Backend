e24e99d3c03aae75fc6b6b2ad014fda9fa3db1ac
Lukman-01 <abdulyekeenlukman@gmail.com>
2023-08-17 15:27:57 +0100

    signup done

--- src/app.js
+++ src/app.js
@@ -2,11 +2,14 @@ const express = require("express");
 const bodyParser = express.json;
 const cors = require("cors");
 require("./config/db");
+const routes = require("./routes");
 
 const app = express();
 
-app.use(bodyParser());
 app.use(cors());
+app.use(bodyParser());
+app.use("/api/v1", routes);
+
 
 module.exports = app;
 
